ddf5b3a9b2ab6ddd1c885e852a5f3c53b93c637b
Mah <mahtabvariyani@gmail.com>
2023-10-11 16:21:15 +0200

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Router } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import MainNavbar from "./components/Navbar/MAinNAbar";
 import Labs from "./components/Labs";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/Footer/Footer";
 function App() {
   return (
